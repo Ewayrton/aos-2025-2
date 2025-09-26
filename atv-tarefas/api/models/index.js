491e65def88c680a19e75b7246aa116e8cf00bd9
Ewayrton Brito de Oliveira <ewayrtonbrito@gmail.com>
2025-09-26 14:07:41 -0300

    alterando index.js models

--- atv-tarefas/api/models/index.js
+++ atv-tarefas/api/models/index.js
@@ -1,5 +1,7 @@
 import Sequelize from "sequelize";
+import pg from "pg";
 import getTarefaModel from "./Tarefa.js";
+
 
 // !!! Simular a conexão com um banco de dados em memória !!! \\
 //const sequelize = new Sequelize("database", "username", "password", {
@@ -7,9 +9,15 @@ import getTarefaModel from "./Tarefa.js";
 //  storage: ":memory:",
 //  logging: false, 
 //});
+
+if (!process.env.POSTGRES_URL) {
+  throw new Error('A variável de ambiente POSTGRES_URL não está definida.');
+}
+
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: "postgres",
   protocol: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
