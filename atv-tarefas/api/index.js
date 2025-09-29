565158ccfc5967b4902144f01623b18692ccd0da
Ewayrton Brito de Oliveira <ewayrtonbrito@gmail.com>
2025-09-29 19:04:57 -0300

    reconfigurando para não mais apagar o banco de dados, e se caso a tabela não exista, criar

--- atv-tarefas/api/index.js
+++ atv-tarefas/api/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import tarefasRoutes from './routes/tarefasRoutes.js';
+import models, { sequelize } from './models';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,14 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use('/tarefas', tarefasRoutes);
+
+const eraseDatabaseOnSync = process.env.ERASE_DATABASE === 'true';
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}!`);
+  });
+});
 
 export default app;
 //app.listen(port, () => {
